d93d8e2088a0f4e2633e3710304a5cc30d637d6f
Immanuel Häussermann <immanuel.haeussermann@screenconcept.ch>
2011-03-02 15:35:52 +0100

    add todo for page parts

--- app/models/kuhsaft/page_parts/base.rb
+++ app/models/kuhsaft/page_parts/base.rb
@@ -1,6 +1,11 @@
 module Kuhsaft
   module PagePart
     class Base
+      
+      #
+      # TODO: behave like ActiveModel for validations etc
+      # http://yehudakatz.com/2010/01/10/activemodel-make-any-ruby-object-feel-like-activerecord/
+      #
       def initialize hash
         hash.each_pair do |k,v|
           self.send "#{k}=", v
