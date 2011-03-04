d318f18c0233266fe148b9c3f1e6b4baa738b02c
Felipe Kaufmann <felipekaufmann@gmail.com>
2011-03-04 15:43:42 +0100

    extend page part

--- app/models/kuhsaft/page_parts/base.rb
+++ app/models/kuhsaft/page_parts/base.rb
@@ -1,7 +1,17 @@
 module Kuhsaft
   module PagePart
+
+    def self.all
+      descendants = []
+      ObjectSpace.each_object(Class) do |k|
+        descendants.unshift k if k < self
+      end
+      descendants.uniq!
+      descendants.map { |d| d.to_s.underscore.to_sym }
+    end
+
     class Base
-      
+
       #
       # TODO: behave like ActiveModel for validations etc
       # http://yehudakatz.com/2010/01/10/activemodel-make-any-ruby-object-feel-like-activerecord/
