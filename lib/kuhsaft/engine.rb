bec811688b589c144fb998fa941667cd8f6b6220
Immanuel Häussermann <immanuel.haeussermann@screenconcept.ch>
2011-03-04 13:47:33 +0100

    add js and basic styles

--- lib/kuhsaft/engine.rb
+++ lib/kuhsaft/engine.rb
@@ -1,8 +1,14 @@
 require 'rails'
 module Kuhsaft
   class Engine < Rails::Engine
-    initializer "kuhsaft.init_stylesheets" do |app|
+    initializer 'kuhsaft.init_stylesheets' do |app|
       Sass::Plugin.add_template_location File.join(Kuhsaft::Engine.root, 'app', 'stylesheets'), File.join(Rails.root, 'public', 'stylesheets')
-   end
+    end
+    
+    initializer 'kuhsaft.static_assets' do |app|
+      if app.config.serve_static_assets
+        app.middleware.insert 0, ::ActionDispatch::Static, "#{root}/public"
+      end
+    end
   end
 end
