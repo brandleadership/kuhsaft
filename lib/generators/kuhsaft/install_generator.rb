91f8334b8d2ac08b052f0169cf26b259cbe72a22
Felipe Kaufmann <felipekaufmann@gmail.com>
2011-03-15 10:38:24 +0100

    fix migration template handling

--- lib/generators/kuhsaft/install_generator.rb
+++ lib/generators/kuhsaft/install_generator.rb
@@ -5,8 +5,8 @@ module Kuhsaft
   class InstallGenerator < Rails::Generators::Base
     include Rails::Generators::Migration
 
-    Kuhsaft::InstallGenerator.source_root(File.expand_path(Kuhsaft::Engine.root + '/db/migrate'))
-    
+   Kuhsaft::InstallGenerator.source_root(File.join(Kuhsaft::Engine.root, '/lib/templates/kuhsaft/install'))
+
     def self.next_migration_number(dirname)
       if ActiveRecord::Base.timestamped_migrations
         Time.now.utc.strftime("%Y%m%d%H%M%S")
@@ -14,10 +14,10 @@ module Kuhsaft
         "%.3d" % (current_migration_number(dirname) + 1)
       end
     end
-    
+
     def create_migration_file
       migration_template 'create_kuhsaft_pages.rb', 'db/migrate/create_kuhsaft_pages.rb'
-      migration_template 'kuhsaft_localized_pages.rb', 'db/migrate/create_kuhsaft_localized_pages.rb'
+      migration_template 'create_kuhsaft_localized_pages.rb', 'db/migrate/create_kuhsaft_localized_pages.rb'
       migration_template 'create_page_part_contents.rb', 'db/migrate/create_page_part_contents.rb'
       migration_template 'create_assets.rb', 'db/migrate/create_assets.rb'
     end
