d0a7e47cc9487ddfd794534dc9125b432b7b665e
Felipe Kaufmann <felipekaufmann@gmail.com>
2011-03-15 10:24:36 +0100

    set template path, refactor

--- lib/generators/kuhsaft/install_generator.rb
+++ lib/generators/kuhsaft/install_generator.rb
@@ -4,6 +4,8 @@ require 'rails/generators/migration'
 module Kuhsaft
   class InstallGenerator < Rails::Generators::Base
     include Rails::Generators::Migration
+
+    Kuhsaft::InstallGenerator.source_root(File.expand_path('../../db/migrate'))
     
     def self.next_migration_number(dirname)
       if ActiveRecord::Base.timestamped_migrations
@@ -14,10 +16,10 @@ module Kuhsaft
     end
     
     def create_migration_file
-      migration_template File.expand_path('../../db/migrate/create_kuhsaft_pages.rb'), 'db/migrate/create_kuhsaft_pages.rb'
-      migration_template File.expand_path('../../db/migrate/create_kuhsaft_localized_pages.rb'), 'db/migrate/create_kuhsaft_localized_pages.rb'
-      migration_template File.expand_path('../../db/migrate/create_page_part_contents.rb'), 'db/migrate/create_page_part_contents.rb'
-      migration_template File.expand_path('../../db/migrate/create_assets.rb'), 'db/migrate/create_assets.rb'
+      migration_template 'create_kuhsaft_pages.rb', 'db/migrate/create_kuhsaft_pages.rb'
+      migration_template 'kuhsaft_localized_pages.rb', 'db/migrate/create_kuhsaft_localized_pages.rb'
+      migration_template 'create_page_part_contents.rb', 'db/migrate/create_page_part_contents.rb'
+      migration_template 'create_assets.rb', 'db/migrate/create_assets.rb'
     end
   end
 end
