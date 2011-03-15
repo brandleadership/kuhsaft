395208939e9628ea412a7558d43ad4514cfa2774
Felipe Kaufmann <felipekaufmann@gmail.com>
2011-03-15 17:09:25 +0100

    serve static engine assets

--- lib/kuhsaft/engine.rb
+++ lib/kuhsaft/engine.rb
@@ -4,13 +4,11 @@ module Kuhsaft
     initializer 'kuhsaft.init_stylesheets' do |app|
       Sass::Plugin.add_template_location File.join(Kuhsaft::Engine.root, 'app', 'stylesheets'), File.join(Rails.root, 'public', 'stylesheets')
     end
-    
+
     initializer 'kuhsaft.static_assets' do |app|
-      if app.config.serve_static_assets
-        app.middleware.insert 0, ::ActionDispatch::Static, "#{root}/public"
-      end
+      app.middleware.use ::ActionDispatch::Static, "#{root}/public"
     end
-    
+
     initializer 'kuhsaft.helpers' do |app|
       # Include your helpers here or they won't be loaded
       # ActionView::Base.send :include, Kuhsaft::PagesHelper
