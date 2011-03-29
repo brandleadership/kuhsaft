5efe6fd5ac316c8b89584a12d24c0b10119a4e65
Immanuel Häussermann <immanuel.haeussermann@screenconcept.ch>
2011-03-29 11:30:41 +0200

    set translation locales in controller specs

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,14 +2,19 @@ require 'spec_helper'
 
 describe Kuhsaft::PagesController do
   render_views
-
+  
+  before do
+    set_lang :en
+    # create page with slug=english-title
+    @page = Factory.create :page
+  end
+  
+  after do
+    Kuhsaft::Page.all.each { |p| p.destroy }
+    reset_lang
+  end
+  
   describe 'should render successfully' do
-    
-    before :all do
-      # create page with slug=english-title
-      Factory.create :page
-    end
-    
     it '#show' do
       get :show, :url => '/en/english-title'
       response.should be_success
