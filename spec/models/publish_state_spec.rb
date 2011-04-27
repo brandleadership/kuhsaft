9700eeda2f00e806ccf8d0f40141dcbe0bce4ad8
Immanuel Häussermann <immanuel.haeussermann@screenconcept.ch>
2011-04-27 15:51:23 +0200

    spec publish_state

---
+++ spec/models/publish_state_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+
+describe Kuhsaft::PublishState do
+  context 'unpublished' do
+    before do
+      @publish_state = Kuhsaft::PublishState.new(:name => 'unpublished', :value => Kuhsaft::PublishState::UNPUBLISHED)
+    end
+    
+    it 'should be UNPUBLISHED' do
+      @publish_state.value.should be(Kuhsaft::PublishState::UNPUBLISHED)
+    end
+    
+    it 'should have a translated name' do
+      @publish_state.human_name.should eq(I18n.translate('unpublished'))
+    end
+  end
+  
+  context 'published' do
+    before do
+      @publish_state = Kuhsaft::PublishState.new(:name => 'published', :value => Kuhsaft::PublishState::PUBLISHED)
+    end
+    
+    it 'should be PUBLISHED' do
+      @publish_state.value.should be(Kuhsaft::PublishState::PUBLISHED)
+    end
+    
+    it 'should have a translated name' do
+      @publish_state.human_name.should eq(I18n.translate('published'))
+    end
+  end
+  
+  context 'published_at' do
+    before do
+      @publish_state = Kuhsaft::PublishState.new(:name => 'published_at', :value => Kuhsaft::PublishState::PUBLISHED_AT)
+    end
+    
+    it 'should be PUBLISHED_AT' do
+      @publish_state.value.should be(Kuhsaft::PublishState::PUBLISHED_AT)
+    end
+    
+    it 'should have a translated name' do
+      @publish_state.human_name.should eq(I18n.translate('published_at'))
+    end
+  end
+end
