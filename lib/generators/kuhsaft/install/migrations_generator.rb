5c416f55e45aa52ca6eedfa1c21f5571256c1fc4
Immanuel Häussermann <immanuel.haeussermann@screenconcept.ch>
2011-04-28 10:19:09 +0200

    migration generator will only install new migrations when updating

--- lib/generators/kuhsaft/install/migrations_generator.rb
+++ lib/generators/kuhsaft/install/migrations_generator.rb
@@ -14,15 +14,27 @@ module Kuhsaft
       end
 
       def create_migration_file
-        migration_template 'create_kuhsaft_pages.rb', Rails.root + 'db/migrate/create_kuhsaft_pages.rb'
-        migration_template 'create_kuhsaft_localized_pages.rb', Rails.root + 'db/migrate/create_kuhsaft_localized_pages.rb'
-        migration_template 'create_page_part_contents.rb', Rails.root + 'db/migrate/create_page_part_contents.rb'
-        migration_template 'create_assets.rb', Rails.root + 'db/migrate/create_assets.rb'
-        migration_template 'add_url_to_localized_pages.rb', Rails.root + 'db/migrate/add_url_to_localized_pages.rb'
-        migration_template 'add_type_to_page_part_contents.rb', Rails.root + 'db/migrate/add_type_to_page_part_contents.rb'
-        migration_template 'add_fulltext_to_localized_page.rb', Rails.root + 'db/migrate/add_fulltext_to_localized_page.rb'
-        migration_template 'add_page_type_to_localized_pages.rb', Rails.root + 'db/migrate/add_page_type_to_localized_pages.rb'
-        migration_template 'add_published_at_to_localized_pages.rb', Rails.root + 'db/migrate/add_published_at_to_localized_pages.rb'
+        install_migration_once 'create_kuhsaft_pages.rb'
+        install_migration_once 'create_kuhsaft_localized_pages.rb'
+        install_migration_once 'create_page_part_contents.rb'
+        install_migration_once 'create_assets.rb'
+        install_migration_once 'add_url_to_localized_pages.rb'
+        install_migration_once 'add_type_to_page_part_contents.rb'
+        install_migration_once 'add_fulltext_to_localized_page.rb'
+        install_migration_once 'add_page_type_to_localized_pages.rb'
+        install_migration_once 'add_published_at_to_localized_pages.rb'
+      end
+      
+      private      
+      def install_migration_once file
+        end_path = File.join(Rails.root, 'db/migrate/')
+        end_file = File.join(end_path, file)
+        installed = Dir[File.join(end_path, '*.rb')].map(&:to_s).select { |f| f.include?(file) }.length > 0
+        if installed
+          log "The migration '#{file}' is already installed in '#{end_path}'"
+        else
+          migration_template(file, end_file)
+        end
       end
     end
   end
