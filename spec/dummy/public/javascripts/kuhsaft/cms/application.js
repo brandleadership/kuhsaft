1aa6ade2d7890639f7ad228a33f3c101ed29e9a3
Immanuel Häussermann <immanuel.haeussermann@screenconcept.ch>
2011-05-12 14:38:16 +0200

    implement styling and js for sortable page parts

--- spec/dummy/public/javascripts/kuhsaft/cms/application.js
+++ spec/dummy/public/javascripts/kuhsaft/cms/application.js
@@ -1,14 +1,32 @@
 $(function(){
-  $('.has-datepicker').datepicker({
-    dateFormat: 'dd.mm.yy'
-  })
-  $("ul.sortable").sortable({
-    containment: 'parent',
-    update: function(event, ui){
-      var self = $(ui.item),
+  function updateSortable(event, ui){
+    var self = $(ui.item),
           prev = self.prev(),
           id   = prev.length > 0 ? prev.data('id') : ''
       $.post(self.data('put-url'), {reposition:id, _method: 'put'})
-    }
+  }
+  
+  $("ul.sortable").sortable({
+    containment: 'parent',
+    update: updateSortable
+  })
+  
+  $(".page-part-list").sortable({
+    items: '.draggable-box',
+    opacity: 0.3,
+    containment: 'parent',
+    handle: '.drag-handler',
+    update: updateSortable
+  })
+  
+  $(".page-part-list .drag-handler").mouseenter(function(){
+    $(this).parent().addClass("will-drag")
+  })
+  .mouseleave(function(){
+    $(this).parent().removeClass("will-drag")
+  })
+  
+  $(".page-part-list .draggable-box").mouseleave(function(){
+    $(this).removeClass("will-drag")
   })
 })
