d3308c5d536bc4a126c1f27cdb32df3280352d62
Immanuel Häussermann <immanuel.haeussermann@screenconcept.ch>
2012-11-12 17:23:00 +0100

    upgrade factories to new factory girl version

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,21 +1,23 @@
-Factory.define :localized_page, :class => Kuhsaft::LocalizedPage do |p|
-  p.locale 'en'
-  p.title { "English Title #{Factory.next(:title)}" }
-  p.published 1
-  p.body 'hi'
-  p.url ''
-  p.association :page
-end
+FactoryGirl.define do
+  sequence(:title) { |n| n }
 
-Factory.define :page, :class => Kuhsaft::Page do |p|
-  p.position 1
-  p.after_create do |page|
-    page.localized_pages << Factory.create(:localized_page, :page => page)
+  factory :localized_page, :class => Kuhsaft::LocalizedPage do |p|
+    p.locale 'en'
+    p.title { "English Title #{Factory.next(:title)}" }
+    p.published 1
+    p.body 'hi'
+    p.url ''
+    p.association :page
+  end
+
+  factory :page, :class => Kuhsaft::Page do |p|
+    p.position 1
+    p.after_create do |page|
+      page.localized_pages << Factory.create(:localized_page, :page => page)
+    end
+  end
+
+  factory :asset, :class => Kuhsaft::Asset do |a|
+    a.file File.open("#{Kuhsaft::Engine.root}/spec/dummy/app/assets/images/spec-image.png")
   end
 end
-
-Factory.define :asset, :class => Kuhsaft::Asset do |a|
-  a.file File.open("#{Kuhsaft::Engine.root}/spec/dummy/app/assets/images/spec-image.png")
-end
-
-Factory.sequence(:title) { |n| n }
