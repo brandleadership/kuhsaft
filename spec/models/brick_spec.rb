61712c32c6d08cd3aec577916cd56012ffb9d761
Immanuel Häussermann <immanuel.haeussermann@screenconcept.ch>
2012-12-14 11:27:49 +0100

    implement parent chain access in brick

--- spec/models/brick_spec.rb
+++ spec/models/brick_spec.rb
@@ -7,4 +7,19 @@ describe Kuhsaft::Brick do
       expect { Kuhsaft::Brick.new }.to raise_error
     end
   end
+
+  describe '#render_stacked?' do
+    it 'returns false by default' do
+      Kuhsaft::Brick.new.render_stacked?.should be_false
+    end
+  end
+
+  describe '#parents' do
+    it 'returns the chain of parents' do
+      item1, item2, item3 = mock, mock, Kuhsaft::Brick.new
+      item2.stub(:brick_list).and_return(item1)
+      item3.stub(:brick_list).and_return(item2)
+      item3.parents.should == [item1, item2]
+    end
+  end
 end
