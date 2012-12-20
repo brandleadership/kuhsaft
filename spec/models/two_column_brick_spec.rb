9805939af0eba611e80685a879d8bf49c311db7a
Immanuel Häussermann <immanuel.haeussermann@screenconcept.ch>
2012-12-20 15:25:09 +0100

    add spec for two column brick

---
+++ spec/models/two_column_brick_spec.rb
@@ -0,0 +1,54 @@
+require 'spec_helper'
+
+describe Kuhsaft::TwoColumnBrick do
+
+  let :two_column_brick do
+    Kuhsaft::TwoColumnBrick.new
+  end
+
+  describe '#user_can_add_childs?' do
+    it 'returns false' do
+      two_column_brick.user_can_add_childs?.should be_false
+    end
+  end
+
+  describe '#user_can_change_persisted?' do
+    it 'returns true' do
+      two_column_brick.user_can_change_persisted?.should be_true
+    end
+  end
+
+  describe '#renders_own_childs?' do
+    it 'returns true' do
+      two_column_brick.renders_own_childs?.should be_true
+    end
+  end
+
+  describe '#partitioning' do
+    context 'when no partition is set' do
+      it 'returns 0 (50/50)' do
+        two_column_brick.partitioning.should be(0)
+      end
+    end
+
+    context 'when the partition is set' do
+      it 'returns the value' do
+        two_column_brick.partitioning = 1
+        two_column_brick.partitioning.should be(1)
+      end
+    end
+  end
+
+  describe '.partitionings' do
+    it 'returns the 3 default partitions' do
+      Kuhsaft::TwoColumnBrick.partitionings.should have(3).items
+    end
+  end
+
+  describe '#create' do
+    it 'creates two single columns as childs' do
+      two_column_brick.save
+      two_column_brick.bricks.should be_all { |brick| brick.should be_a(Kuhsaft::ColumnBrick) }
+    end
+  end
+end
