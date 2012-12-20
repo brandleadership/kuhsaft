408d7ac5fe810e8931c51c9181f89bb870fdbfa0
Immanuel Häussermann <immanuel.haeussermann@screenconcept.ch>
2012-12-20 17:27:16 +0100

    add basic dropdown functionality from brick type

--- app/models/kuhsaft/brick_type.rb
+++ app/models/kuhsaft/brick_type.rb
@@ -1,6 +1,7 @@
 module Kuhsaft
   class BrickType < ActiveRecord::Base
     attr_accessible :disabled, :class_name, :group
-    scope :by_group, order('group, id')
+    scope :grouped, order('`group`, `id` asc')
+    scope :enabled, where('disabled IS NOT false')
   end
 end
