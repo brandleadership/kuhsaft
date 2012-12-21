3f1184a2c95213ffabf3724153d0849e6052700b
Immanuel Häussermann <immanuel.haeussermann@screenconcept.ch>
2012-12-21 13:53:50 +0100

    add i18n configuration for backend

--- lib/kuhsaft/engine.rb
+++ lib/kuhsaft/engine.rb
@@ -2,19 +2,8 @@ module Kuhsaft
   class Engine < ::Rails::Engine
     isolate_namespace Kuhsaft
 
-    # initializer 'kuhsaft.init_stylesheets' do |app|
-    #   Sass::Plugin.add_template_location File.join(Kuhsaft::Engine.root, 'app', 'stylesheets'), File.join(Rails.root, 'public', 'stylesheets')
-    # end
-
-    # initializer 'kuhsaft.static_assets' do |app|
-    #   app.middleware.use ::ActionDispatch::Static, "#{Kuhsaft::Engine.root}/public"
-    # end
-
-    # initializer 'kuhsaft.helpers' do |app|
-    #   # Include your helpers here or they won't be loaded
-    #   ActionView::Base.send :include, Kuhsaft::PagesHelper
-    #   ActionView::Base.send :include, Kuhsaft::CmsHelper
-    # end
+    config.i18n.fallbacks = [:de]
+    config.i18n.load_path += Dir[Kuhsaft::Engine.root.join('config', 'locales', '**', '*.{yml}').to_s]
 
   end
 end
