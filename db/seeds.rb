37f1a381673f0d1ef184b9b70d9ce8a8ebcda9f2
Immanuel Häussermann <immanuel.haeussermann@screenconcept.ch>
2013-01-11 15:58:38 +0100

    document migration for accordion bricks, add brick types to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,6 @@ Kuhsaft::Cms::Admin.create(:email => 'admin@screenconcept.ch', :password => 'bam
 
 Kuhsaft::BrickType.create(:class_name => 'Kuhsaft::TextBrick', :group => 'elements')
 Kuhsaft::BrickType.create(:class_name => 'Kuhsaft::LinkBrick', :group => 'elements')
+Kuhsaft::BrickType.create(:class_name => 'Kuhsaft::AccordionBrick', :group => 'elements')
+Kuhsaft::BrickType.create(:class_name => 'Kuhsaft::AccordionItemBrick', :group => 'elements')
 Kuhsaft::BrickType.create(:class_name => 'Kuhsaft::TwoColumnBrick', :group => 'layout_elements')
