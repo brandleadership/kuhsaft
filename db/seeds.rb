63a37a89ee310d1b18c9a722c02dc1aa72db32d9
Felipe Kaufmann <felipekaufmann@gmail.com>
2013-01-14 12:47:46 +0100

    add slider brick

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,5 @@ Kuhsaft::Cms::Admin.create(:email => 'admin@screenconcept.ch', :password => 'bam
 Kuhsaft::BrickType.create(:class_name => 'Kuhsaft::TextBrick', :group => 'elements')
 Kuhsaft::BrickType.create(:class_name => 'Kuhsaft::LinkBrick', :group => 'elements')
 Kuhsaft::BrickType.create(:class_name => 'Kuhsaft::TwoColumnBrick', :group => 'layout_elements')
+Kuhsaft::BrickType.create(:class_name => 'Kuhsaft::SliderBrick', :group => 'elements')
+Kuhsaft::BrickType.create(:class_name => 'Kuhsaft::ImageBrick', :group => 'elements')
