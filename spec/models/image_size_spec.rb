93c678b9fe581d35d47d1d232a68b7075e8f0c43
Immanuel Häussermann <immanuel.haeussermann@screenconcept.ch>
2013-01-22 17:18:36 +0100

    refactor image size class methods

--- spec/models/image_size_spec.rb
+++ spec/models/image_size_spec.rb
@@ -1,10 +1,30 @@
 require 'spec_helper'
 
 describe Kuhsaft::ImageSize do
-  describe '.all' do
-    it 'returns the default sizes' do
+  before do
+    Kuhsaft::ImageSize.build_defaults!
+  end
+
+  describe '.build_defaults!' do
+    it 'sets the default sizes' do
       Kuhsaft::ImageSize.all.should == [Kuhsaft::ImageSize.gallery_size,
                                         Kuhsaft::ImageSize.teaser_size]
+    end
+  end
+
+  describe '.clear!' do
+    before do
+      Kuhsaft::ImageSize.clear!
+    end
+
+    it 'empties the list' do
+      Kuhsaft::ImageSize.all.should be_empty
+    end
+  end
+
+  describe '.add' do
+    it 'adds a new image size' do
+      expect { Kuhsaft::ImageSize.add(:stuff, 200, 100) }.to change(Kuhsaft::ImageSize.all, :count).by(1)
     end
   end
 
