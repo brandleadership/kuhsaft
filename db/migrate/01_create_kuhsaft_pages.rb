f0e08716bd0d33366802829a7e9b7df17a4a7d6b
Felipe Kaufmann <felipekaufmann@gmail.com>
2013-01-23 12:17:02 +0100

    shoestrap integration
    - create _de fields for page as well since shoe-
      strap uses :de as default locale
    
    - extract navbar into partial that can be over-
      ridden in the app
    
    - use bootstrap responsive css… makes the UI
      barely functional on mobiles

--- db/migrate/01_create_kuhsaft_pages.rb
+++ db/migrate/01_create_kuhsaft_pages.rb
@@ -4,15 +4,22 @@ class CreateKuhsaftPages < ActiveRecord::Migration
     create_table :kuhsaft_pages do |t|
       t.integer :position
       t.string :title_en
+      t.string :title_de
       t.string :slug_en
+      t.string :slug_de
       t.string :keywords_en
+      t.string :keywords_de
       t.text :description_en
+      t.text :description_de
       t.text :body_en
+      t.text :body_de
       t.integer :published
       t.references :page
       t.text :url_en
+      t.text :url_de
       t.string :page_type
       t.text :fulltext_en
+      t.text :fulltext_de
       t.string :ancestry
       t.timestamps
     end
