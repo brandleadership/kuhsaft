ce5403d9f94147cc5be8a6c6c31d5b9c0f782746
Immanuel Häussermann <immanuel.haeussermann@screenconcept.ch>
2013-02-25 11:52:31 +0100

    actually fixes #95

--- app/helpers/kuhsaft/cms/pages_helper.rb
+++ app/helpers/kuhsaft/cms/pages_helper.rb
@@ -3,11 +3,15 @@ module Kuhsaft
     module PagesHelper
 
       def content_tab_active(page)
-        :active if page.persisted?
+        if page.errors.blank?
+          :active if page.persisted?
+        end
       end
 
       def metadata_tab_active(page)
-        :active unless page.persisted?
+        if page.errors.present? || !page.persisted?
+          :active
+        end
       end
     end
   end
