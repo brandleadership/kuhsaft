28a86653df610eb6a3d361c7ad418828361fcc3a
Phil Schilter <philipp.schilter@screenconcept.ch>
2013-03-14 13:40:59 +0100

    add before save hook for stripping empty p tags

--- app/models/kuhsaft/text_brick.rb
+++ app/models/kuhsaft/text_brick.rb
@@ -2,6 +2,12 @@ module Kuhsaft
   class TextBrick < Brick
     attr_accessible :text, :read_more_text
 
+    before_save :strip_read_more_text
+
+    def strip_read_more_text
+      read_more_text.clear if Digest::MD5.hexdigest(read_more_text) == "9ec94c03b98c5b356364006bf4e73dce"
+    end
+
     def user_can_add_childs?
       false
     end
