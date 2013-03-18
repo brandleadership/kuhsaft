2ff36397720c826fc99d36e4c904622926ff00b6
Felipe Kaufmann <felipekaufmann@gmail.com>
2013-03-18 17:17:17 +0100

    ugly workaround with ugly spec for ugly problem

--- app/models/kuhsaft/text_brick.rb
+++ app/models/kuhsaft/text_brick.rb
@@ -4,16 +4,27 @@ module Kuhsaft
 
     before_save :strip_read_more_text
 
-    def strip_read_more_text
-      read_more_text.clear if Digest::MD5.hexdigest(read_more_text) == "9ec94c03b98c5b356364006bf4e73dce"
-    end
-
     def user_can_add_childs?
       false
     end
 
     def collect_fulltext
       [super, text, read_more_text].join(' ')
+    end
+
+    private
+
+    # Clear read_more_text field if it just contains an empty p tag
+    #
+    # readctor.js injects an almost empty p-tag into empty form fields.
+    # However, instead of being really empty, it currently contains the &#8203; (Zero width space)
+    # Character.
+    def strip_read_more_text
+      read_more_text.clear if text_contains_funky_chars
+    end
+
+    def text_contains_funky_chars
+      read_more_text && read_more_text.strip.hash == -1282209220116339654
     end
   end
 end
