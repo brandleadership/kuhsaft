4355158b5a2b90c1203c3aa36dd939bb90b7403a
Felipe Kaufmann <felipekaufmann@gmail.com>
2013-04-09 12:00:20 +0200

    add ckeditor config file to installer

--- lib/generators/kuhsaft/assets/install_generator.rb
+++ lib/generators/kuhsaft/assets/install_generator.rb
@@ -8,10 +8,14 @@ module Kuhsaft
       def copy_customizations
         custom_css_folder = 'app/assets/stylesheets/kuhsaft/cms/'
         custom_js_folder = 'app/assets/javascripts/kuhsaft/cms/'
+
         empty_directory custom_css_folder
         empty_directory custom_js_folder
+
         copy_file 'customizations.css.sass', "#{custom_css_folder}/customizations.css.sass"
         copy_file 'customizations.js.coffee', "#{custom_js_folder}/customizations.js.coffee"
+        copy_file 'ck-config.js.coffee', "#{custom_js_folder}/ck-config.js.coffee"
+
         inject_into_file 'config/environments/production.rb', :after => /config\.assets\.precompile.*$/ do
           "\n  config.assets.precompile += %w( kuhsaft/cms/customizations.css kuhsaft/cms/customizations.js )"
         end
