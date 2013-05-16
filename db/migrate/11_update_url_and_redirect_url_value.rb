9a668b9a4e690bb0fc6227471103b8de2eb2e5f3
ryabrody <franca.rast@screenconcept.ch>
2013-05-16 11:08:48 +0200

    create migration to update redirect_pages

---
+++ db/migrate/11_update_url_and_redirect_url_value.rb
@@ -0,0 +1,39 @@
+class UpdateUrlAndRedirectUrlValue < ActiveRecord::Migration
+  def up
+    @redirect_pages = Kuhsaft::Page.where(:page_type => 'redirect')
+    I18n.available_locales.each do |locale|
+      move_url_to_redirect_url(locale)
+    end
+  end
+
+  def down
+    @redirect_pages = Kuhsaft::Page.where(:page_type => 'redirect')
+    I18n.available_locales.each do |locale|
+      move_redirect_url_to_url(locale)
+    end
+
+  end
+
+  def move_url_to_redirect_url(locale)
+    I18n.with_locale(locale) do
+      @redirect_pages.each do |redirect_page|
+        redirect_url = redirect_page.url
+        redirect_page.update_attributes(
+          :url => nil,
+          :redirect_url => redirect_url
+        ) if redirect_page
+      end
+    end
+  end
+
+  def move_redirect_url_to_url(locale)
+    I18n.with_locale(locale) do
+      @redirect_pages.each do |redirect_page|
+        url = redirect_page.redirect_url
+        redirect_page.update_attributes(
+          :url => url
+        ) if redirect_page
+      end
+    end
+  end
+end
