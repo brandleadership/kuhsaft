9541c04498d93ed799abdb33e786f7963e7cd75d
Felipe Kaufmann <felipekaufmann@gmail.com>
2013-07-10 16:51:58 +0200

    first draft of update pagetree method

---
+++ lib/kuhsaft/page_tree.rb
@@ -0,0 +1,15 @@
+module Kuhsaft
+  module PageTree
+    extend self
+
+    def update(params, parent_id = nil)
+      params.each do |page_position, page_content|
+        page = Page.find(page_content['id'])
+        page.update_attributes(parent_id: parent_id, position: page_position)
+        unless page_content['children'].nil?
+          update(page_content['children'], page.id)
+        end
+      end
+    end
+  end
+end
