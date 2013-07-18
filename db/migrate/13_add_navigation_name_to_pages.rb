b753f3bfd0b32add5558f68843b7dcc9dfe876f5
donaier <donat.baier@screenconcept.ch>
2013-07-18 11:43:09 +0200

    cleanup migrations

--- db/migrate/13_add_navigation_name_to_pages.rb
+++ db/migrate/13_add_navigation_name_to_pages.rb
@@ -3,5 +3,16 @@ class AddNavigationNameToPages < ActiveRecord::Migration
     I18n.available_locales.each do |locale|
       add_column :kuhsaft_pages, "navigation_name_#{locale}", :string
     end
+    move_title_data_to_navigation_name
+  end
+end
+
+def move_title_data_to_navigation_name
+  I18n.available_locales.each do |locale|
+    Kuhsaft::Page.all.each do |page|
+      page.navigation_name = page.title
+      page.title = nil
+      page.save
+    end
   end
 end
