f645e5e2f8ea4d28a2c853ca6cd8eb0cbb18397d
Noëlle Rosenberg <noelle.rosenberg@screenconcept.ch>
2013-07-18 12:11:57 +0200

    remove duplicated specs

--- spec/controllers/kuhsaft/pages_controller_spec.rb
+++ spec/controllers/kuhsaft/pages_controller_spec.rb
@@ -22,30 +22,6 @@ describe Kuhsaft::PagesController do
   end
 
   describe '#show' do
-    describe 'redirect' do
-      around(:each) do |example|
-        I18n.with_locale :de do
-          example.run
-        end
-      end
-
-      context 'when page is not a redirect page' do
-        it 'responds with page' do
-          page = FactoryGirl.create(:page, :slug => 'dumdidum', :url => 'de/dumdidum')
-          get :show,  { :url => page.slug, :use_route => :kuhsaft }
-          assigns(:page).should eq(page)
-        end
-      end
-
-      context 'when page is a redirect page' do
-        it 'redirects to the redirected url' do
-          page = FactoryGirl.create(:page, :page_type => 'redirect', :slug => 'dumdidum', :url => 'de/dumdidum', :redirect_url => 'de/redirect_page')
-          get :show,  { :url => page.slug, :use_route => :kuhsaft }
-          expect(response).to redirect_to("/de/redirect_page")
-        end
-      end
-    end
-
     describe 'routing' do
       context 'without url' do
         before do
