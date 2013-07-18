2590e93d7be521260d348d710ba31daaacb8c879
donaier <donat.baier@screenconcept.ch>
2013-07-18 13:08:12 +0200

    cleanup irreversible migration

--- db/migrate/13_add_navigation_name_to_pages.rb
+++ db/migrate/13_add_navigation_name_to_pages.rb
@@ -1,18 +1,20 @@
 class AddNavigationNameToPages < ActiveRecord::Migration
-  def change
+  def up
     I18n.available_locales.each do |locale|
       add_column :kuhsaft_pages, "navigation_name_#{locale}", :string
     end
     move_title_data_to_navigation_name
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
   end
 end
 
 def move_title_data_to_navigation_name
   I18n.available_locales.each do |locale|
     Kuhsaft::Page.all.each do |page|
-      page.navigation_name = page.title
-      page.title = nil
-      page.save
+      page.update_attribute(navigation_name: page.title)
     end
   end
 end
