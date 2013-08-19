84883bfc5826ea45b69e5d2b619ae7bc7bbfc704
ryabrody <franca.rast@screenconcept.ch>
2013-08-19 14:46:14 +0200

    let seeds be able to run multiple time without create duplicated data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,20 @@
 # Add default kuhsaft brick types
+brick_types = [
+  { class_name: 'Kuhsaft::TextBrick', group: 'elements' },
+  { class_name: 'Kuhsaft::LinkBrick', group: 'elements' },
+  { class_name: 'Kuhsaft::VideoBrick', group: 'elements' },
+  { class_name: 'Kuhsaft::AccordionBrick', group: 'elements' },
+  { class_name: 'Kuhsaft::AccordionItemBrick', group: 'elements' },
+  { class_name: 'Kuhsaft::TwoColumnBrick', group: 'layout_elements' },
+  { class_name: 'Kuhsaft::SliderBrick', group: 'elements' },
+  { class_name: 'Kuhsaft::ImageBrick', group: 'elements' },
+  { class_name: 'Kuhsaft::PlaceholderBrick', group: 'elements' },
+  { class_name: 'Kuhsaft::AnchorBrick', group: 'elements' },
+  { class_name: 'Kuhsaft::AssetBrick', group: 'elements' }
+]
 
-Kuhsaft::BrickType.create(:class_name => 'Kuhsaft::TextBrick', :group => 'elements')
-Kuhsaft::BrickType.create(:class_name => 'Kuhsaft::LinkBrick', :group => 'elements')
-Kuhsaft::BrickType.create(:class_name => 'Kuhsaft::VideoBrick', :group => 'elements')
-Kuhsaft::BrickType.create(:class_name => 'Kuhsaft::AccordionBrick', :group => 'elements')
-Kuhsaft::BrickType.create(:class_name => 'Kuhsaft::AccordionItemBrick', :group => 'elements')
-Kuhsaft::BrickType.create(:class_name => 'Kuhsaft::TwoColumnBrick', :group => 'layout_elements')
-Kuhsaft::BrickType.create(:class_name => 'Kuhsaft::SliderBrick', :group => 'elements')
-Kuhsaft::BrickType.create(:class_name => 'Kuhsaft::ImageBrick', :group => 'elements')
-Kuhsaft::BrickType.create(:class_name => 'Kuhsaft::PlaceholderBrick', :group => 'elements')
-Kuhsaft::BrickType.create(:class_name => 'Kuhsaft::AnchorBrick', :group => 'elements')
-Kuhsaft::BrickType.create(:class_name => 'Kuhsaft::AssetBrick', :group => 'elements')
+brick_types.each do |bt|
+  brick_type = Kuhsaft::BrickType.find_or_create_by(class_name: bt[:class_name])
+  brick_type.update! bt
+end
+
