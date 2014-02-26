9c26324884bcb3cccbfd2a8d81f7afb87a3dc88c
Franca Rast <franca.rast@screenconcept.ch>
2014-02-26 16:05:34 +0100

    remove whiteline

--- lib/generators/kuhsaft/translations/add_generator.rb
+++ lib/generators/kuhsaft/translations/add_generator.rb
@@ -22,7 +22,6 @@ module Kuhsaft
         locale.downcase.underscore
       end
 
-
       def create_locale_migration_file
         migration_template('add_translation.html.erb', Rails.root.join('db', 'migrate', "add_#{get_locale}_translation.rb"))
       end
