a60e6e54c81420332c5286f1625aea2e6e937bfe
Phil Schilter <phil.schilter@gmail.com>
2014-03-07 17:09:46 +0100

    add Kuhsaft::Api::Pages controller

---
+++ app/controllers/kuhsaft/api/pages_controller.rb
@@ -0,0 +1,14 @@
+module Kuhsaft
+  module Api
+    class PagesController < ActionController::Base
+      layout :false
+      respond_to :json
+
+      def index
+        @pages = Kuhsaft::Page.unscoped.published.content_page
+        render :json => @pages.as_json
+      end
+
+    end
+  end
+end
