a7ff239fd97f73fb014c023cd1476144bddbd7c6
Phil Schilter <phil.schilter@gmail.com>
2014-03-07 17:09:06 +0100

    update ruby hash syntax and add api route

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,28 @@
 Kuhsaft::Engine.routes.draw do
+
   namespace :cms do
-    resources :pages, :except => :show do
-      post :sort, :on => :collection
+    resources :pages, except: :show do
+      post :sort, on: :collection
     end
-    resources :bricks, :except => [:edit, :index] do
-      post :sort, :on => :collection
+
+    resources :bricks, except: [:edit, :index] do
+      post :sort, on: :collection
+    end
+
+    namespace :api, defaults: { format: :json } do
+      resources :pages, only: :index
     end
 
     resources :assets
-    root :to => 'pages#index'
+    root to: 'pages#index'
   end
 
-  scope ":locale", :locale => /#{I18n.available_locales.join('|')}/ do
+  scope ":locale", locale: /#{I18n.available_locales.join('|')}/ do
     resources :pages,
-      :only => [:index],
-      :defaults => { :locale => I18n.locale }
-    get '(*url)' => 'pages#show', :as => :page
+      only: [:index],
+      defaults: { locale: I18n.locale }
+    get '(*url)' => 'pages#show', as: :page
   end
 
-  get '/sitemap.:format' => 'sitemaps#index', format: 'xml'
+  get '/sitemap.format' => 'sitemaps#index', format: 'xml'
 end
