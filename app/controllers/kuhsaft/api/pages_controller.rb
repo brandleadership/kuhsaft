6c1690fbcf035db9e67e9cfa9b38e4cae1da4f38
Phil Schilter <phil.schilter@gmail.com>
2014-03-13 15:23:41 +0100

    add order for pages in API

--- app/controllers/kuhsaft/api/pages_controller.rb
+++ app/controllers/kuhsaft/api/pages_controller.rb
@@ -6,7 +6,7 @@ module Kuhsaft
 
       def index
         I18n.locale = params[:locale]
-        @pages = Kuhsaft::Page.unscoped.published.content_page
+        @pages = Kuhsaft::Page.unscoped.published.content_page.order(position: :asc)
         render json: @pages.as_json
       end
     end
