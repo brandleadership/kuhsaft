9a3dc3bc7dfb79dd8904f4444a033871b0eb7542
Felipe Kaufmann <felipekaufmann@gmail.com>
2014-03-26 12:01:17 +0100

    refactor/cleanup

--- app/controllers/kuhsaft/pages_controller.rb
+++ app/controllers/kuhsaft/pages_controller.rb
@@ -1,6 +1,7 @@
 module Kuhsaft
   class PagesController < ::ApplicationController
     respond_to :html
+    before_action :find_page_by_url, only: :show
 
     def index
       @search = params[:search]
@@ -10,10 +11,6 @@ module Kuhsaft
     end
 
     def show
-      url = locale.to_s
-      url += "/#{params[:url]}" if params[:url].present?
-      @page = Kuhsaft::Page.find_by_url(url)
-
       if @page.present? && @page.redirect? && @page.redirect_url.present?
         redirect_url = @page.redirect_url.sub(/\A\/+/, '') # remove all preceding slashes
         redirect_to "/#{redirect_url}"
@@ -22,13 +19,21 @@ module Kuhsaft
       elsif @page.blank? && respond_to?(:handle_404)
         handle_404
       else
-        raise ActionController::RoutingError.new('Not Found')
+        raise ActionController::RoutingError, 'Not Found'
       end
     end
 
     def lookup_by_id
       @page = Page.find(params[:id])
       redirect_to "/#{@page.url}"
+    end
+
+    private
+
+    def find_page_by_url
+      url = locale.to_s
+      url += "/#{params[:url]}" if params[:url].present?
+      @page = Kuhsaft::Page.find_by_url(url)
     end
   end
 end
