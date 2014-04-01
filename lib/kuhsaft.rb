cbdda7f9332d7682b82842e0695fc8c4e9989d23
Felipe Kaufmann <felipekaufmann@gmail.com>
2014-04-01 15:12:09 +0200

    require touch_placeholders

--- lib/kuhsaft.rb
+++ lib/kuhsaft.rb
@@ -6,6 +6,7 @@ module Kuhsaft
   require 'kuhsaft/searchable'
   require 'kuhsaft/brick_list'
   require 'kuhsaft/partial_extractor'
+  require 'kuhsaft/touch_placeholders'
   require 'simple_form'
   require 'carrierwave'
   require 'rdiscount'
