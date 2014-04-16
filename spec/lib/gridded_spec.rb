0f3b3586ca3c017b2eab4f93a53aee6f8aab680a
donaier <donat.baier@screenconcept.ch>
2014-04-16 13:59:07 +0200

    beautify gridded specs

--- spec/lib/gridded_spec.rb
+++ spec/lib/gridded_spec.rb
@@ -8,16 +8,16 @@ describe Kuhsaft::Gridded do
     end
 
     it 'returns an array for the gridded class' do
-      GridClass.available_grid_sizes.should be_kind_of(Array)
+      expect(GridClass.available_grid_sizes).to be_kind_of(Array)
     end
 
     it 'returns false on gridded? if no col count is set' do
-      GridClass.any_instance.stub(:col_count).and_return(0)
+      expect_any_instance_of(GridClass).to receive(:col_count).at_least(:once).and_return(0)
       expect(GridClass.new.gridded?).to be_false
     end
 
     it 'returns true on gridded? if a col count is set' do
-      GridClass.any_instance.stub(:col_count).and_return(10)
+      expect_any_instance_of(GridClass).to receive(:col_count).at_least(:once).and_return(10)
       expect(GridClass.new.gridded?).to be_true
     end
   end
