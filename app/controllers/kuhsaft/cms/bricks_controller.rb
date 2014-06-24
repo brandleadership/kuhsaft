7bb676519ac0a6536e32a5241c55c9e068283bb5
Franca Rast <franca.rast@screenconcept.ch>
2014-06-24 11:09:54 +0200

    remove image_size setting

--- app/controllers/kuhsaft/cms/bricks_controller.rb
+++ app/controllers/kuhsaft/cms/bricks_controller.rb
@@ -5,7 +5,6 @@ module Kuhsaft
 
       def create
         @brick = params[:brick][:type].constantize.create(brick_params)
-        @brick.image_size = ImageSize.all.first.name.to_s
 
         if @brick.valid?
           respond_with @brick do |format|
