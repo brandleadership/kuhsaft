eab8ebb4a7c46cf4913c5820fb55e2f273cce264
Alexander Adam <mail@alexander-adam.info>
2014-07-22 14:37:12 +0200

    add route helper & input heuristic

--- app/helpers/kuhsaft/cms/admin_helper.rb
+++ app/helpers/kuhsaft/cms/admin_helper.rb
@@ -16,6 +16,62 @@ module Kuhsaft
         link_to locale.to_s.upcase, url_for(
           action: action, content_locale: locale)
       end
+
+      def model_attribute_field(resource, column_name, f)
+        if resource.class.respond_to?(:uploaders) && resource.class.uploaders.keys.include?(column_name.to_sym)
+          f.input column_name, as: :file
+        elsif association_class(resource, column_name) == Kuhsaft::Page
+          route_input f, column_name, page_routes
+        elsif %w(site access_level).include?(column_name.to_s) && current_admin.is_global?
+          f.input column_name, collection: ElesaganterWeb.config.country_sites.map { |k, c| [c[:caption], k] },
+                               include_blank: false,
+                               selected: resource.send(column_name)
+        elsif column_name == :link_url || column_name == :link
+          route_input f, column_name, user_selectable_routes
+        elsif column_name == :download_category_id
+          f.association :download_category, label_method: :localized_title, value_method: :id, include_blank: false
+        elsif column_name.to_s.end_with? '_id'
+          f.association column_name.to_s.gsub(/_id$/, '').to_sym
+        elsif column_name == :body
+          f.input column_name, input_html: { class: 'ckeditor text required' }
+        else
+          f.input column_name
+        end
+      end
+
+      def route_input(form, column_name, selectable_routes)
+        list_name = "routes_#{column_name}"
+        haml_tag :datalist, id: list_name, name: list_name do
+          selectable_routes.each { |route| haml_tag :option, value: route }
+        end
+        form.input column_name, input_html: { list: list_name }, as: 'string'
+      end
+
+      def association_class(resource, column_name)
+        association_class = resource.class.reflect_on_all_associations.find { |r| r.name == column_name.to_sym }.try :class_name
+        association_class.present? ? association_class.constantize : nil
+      end
+
+      # here are some route helpers if you need them in your backend
+      def all_selectable_routes
+        (page_routes + rails_routes).uniq.sort
+      end
+
+      def page_routes
+        Kuhsaft::Page.all_urls
+      end
+
+      def rails_routes
+        routes = Rails.application.routes.routes
+        routes = routes.select do |r|
+          r.verb.match('GET') && !r.requirements.blank? &&
+            !r.requirements[:controller].to_s.start_with?('cms') &&
+            !r.required_parts.any? &&
+            !r.requirements[:controller].to_s.start_with?('rails/')
+        end
+
+        routes.map { |r| _routes.url_for(r.requirements.merge only_path: true).strip }.uniq.sort
+      end
     end
   end
 end
